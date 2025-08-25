f577b8a9d6ca956726179e6e5ff97b0f5d5a4328
andry80 <8888appl8888@gmail.com>
2025-08-25 20:41:40 +0300

    lesson18

--- src/main/java/edu/javacore/drawingnumbers/App.java
+++ src/main/java/edu/javacore/drawingnumbers/App.java
@@ -1,10 +1,38 @@
 package edu.javacore.drawingnumbers;
 
-/**
- * Hello world!
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Enter the number: ");
+        DrawingNumbers(r.readLine());
+    }
+
+    public static final String[][] STRINGS = new String[][]{{" @@@@ ","@@  @@","@@  @@","@@  @@"," @@@@ "},
+                                                            {"  @@  "," @@@  ","@ @@  ","  @@  ","@@@@@@"},
+                                                            {" @@@@ ","@   @@","  @@@ "," @@   ","@@@@@@"},
+                                                            {"@@@@@ ","    @@","@@@@@ ","    @@","@@@@@ "},
+                                                            {"@@  @@","@@  @@","@@@@@@","    @@","    @@"},
+                                                            {"@@@@@@","@@    ","@@@@@ ","    @@","@@@@@ "},
+                                                            {" @@@@@","@@    ","@@@@@ ","@@  @@"," @@@@ "},
+                                                            {"@@@@@@","   @@ ","  @@  "," @@   ","@@    "},
+                                                            {" @@@@ ","@@  @@"," @@@@ ","@@  @@"," @@@@ "},
+                                                            {" @@@@@","@@  @@"," @@@@@","    @@"," @@@@ "}};
+
+    public static void DrawingNumbers(String numbers){
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < 5; i++){
+            for(String num : numbers.split("")){
+                builder.append(STRINGS[Integer.parseInt(num)][i]);
+                builder.append(" ");
+            }
+            builder.append("\n");
+        }
+        System.out.println(builder.toString());
     }
 }
